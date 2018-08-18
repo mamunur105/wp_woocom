ad3f66a0053d387aedaf7f3d85ca9e4ac89cf3a8
mamunur105 <rmamunur105@gmail.com>
2018-08-18 16:02:50 +0600

    this

--- woocom/inc/mailfun.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$multiple_recipients = array(
-    'recipient1@example.com',
-    'recipient2@foo.example.com'
-);
-$subj = 'The email subject';
-$body = 'This is the body of the email';
-wp_mail( $multiple_recipients, $subj, $body );
--- woocom/searchform.php
+++
@@ -1,9 +0,0 @@
-
-<div class="sidebar-box mb-40">
-	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	    <div class="search-box">
-	      <input type="text" placeholder="Search entire store here..." class="input-text" name="s">
-	      <button class="search-btn"><i class="fa fa-search"></i></button>
-	    </div>
-	  </form>
-</div>
